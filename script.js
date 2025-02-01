e4fec7f7d4f35bfb7d90e2a244b608a5f9783056
Juanbrero <juaan.b17@hotmail.com>
2025-02-01 19:02:29 -0300

    descargar cv con js y fin de proyecto

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ function select() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+function downloadCV() {
+    const link = document.createElement("a");
+    link.href = "assets/CVBreroJuanM.pdf";
+    link.download = "CVBreroJuanM.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
